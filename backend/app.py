8ef71dcad5306bfe767cbd5c407e1c679ad02547
Rafael-De-Santis <desantisrd7@gmail.com>
2024-09-30 00:12:06 -0700

    trying fo fix the migrations versions folder, itis not create the migration object

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,17 @@
+import sys
+sys.path.append('C:/Users/d4tap/Documents/Code/rdsweb/backend')
+import os
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
-from config.config import rdswebDbConfig
+from config.config import Config
 
 app = Flask(__name__)
 
 # Configuring the app for PostgreSQL and JWT
 
-app.config.from_object(rdswebDbConfig)
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 jwt = JWTManager(app)
--- backend/models/user.py
+++ backend/models/user.py
@@ -1,3 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy
 from app import db
 from datetime import datetime
 
