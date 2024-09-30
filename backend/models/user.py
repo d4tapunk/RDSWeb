b209be68fadc5f2a6f90b160e9b58769736f4be6
Rafael-De-Santis <desantisrd7@gmail.com>
2024-09-29 23:06:30 -0700

    Class user addded

---
+++ backend/models/user.py
@@ -0,0 +1,13 @@
+from app import db
+from datetime import datetime
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(50), nullable=True) 
+    lastname = db.Column(db.String(50), nullable=True)  
+    age = db.Column(db.Integer, nullable=True) 
+    registration_date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
