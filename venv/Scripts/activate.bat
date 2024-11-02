9b7749b280d2f562a45e1fc70b7500444e7d9e69
Rafael-De-Santis <desantisrd7@gmail.com>
2024-11-02 08:58:45 -0700

    reenvironrizingxD

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\d4tap\Documents\Code\rdsweb\venv
+set VIRTUAL_ENV=C:\Users\d4tap\Documents\Code\rdswebAlpha\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
